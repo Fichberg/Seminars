f03d3346d22cf91d526795191d0800c83205e66b
Fichberg <rfichberg@gmail.com>
2017-05-07 18:49:32 -0300

    Fixed commits massive mess

---
+++ ionic/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { AuthService } from './../providers/auth-service';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { Login } from '../pages/login/login';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Login
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Login
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
+  ]
+})
+export class AppModule {}
